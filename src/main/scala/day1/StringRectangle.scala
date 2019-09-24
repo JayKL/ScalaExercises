94407921f696cb10074bf606467373f734e26236
JayKL <Jaykonrad-lee@hotmail.com>
2019-09-24 17:00:14 +0100

    fixed StringRectangle syntax

--- src/main/scala/day1/StringRectangle.scala
+++ src/main/scala/day1/StringRectangle.scala
@@ -2,13 +2,15 @@ package day1
 
 object StringRectangle extends App {
   def printRectangle(printString: String, amountToPrint: Int): Unit = {
-    for (j <- 1 to amountToPrint) {
-      for (i <- 1 to amountToPrint) {
-        print(printString)
+
+    for (j <- 1 to amountToPrint; i <- 1 to amountToPrint) {
+      print(printString)
+      if (i==amountToPrint) {
+        println()
       }
-      println()
     }
   }
+
 
   printRectangle("Kitty", 3)
 
