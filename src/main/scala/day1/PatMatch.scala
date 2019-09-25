f7bc7b9a61a87b404e8faaa8fabea5f4249278c3
JayKL <Jaykonrad-lee@hotmail.com>
2019-09-25 09:26:16 +0100

    integer Addition Pattern Match with multi, zero checks

--- src/main/scala/day1/PatMatch.scala
+++ src/main/scala/day1/PatMatch.scala
@@ -1,9 +1,17 @@
 package day1
 
 object PatMatch extends App{
-  def integerAdditionPatMatch(inputIntOne: Int, inputIntTwo: Int,addOrNot: Boolean): Int = addOrNot match{
-    case addOrNot=> inputIntOne*inputIntTwo
-    case _ => inputIntOne+inputIntTwo
+  def integerAdditionPatMatch(inputIntOne: Int, inputIntTwo: Int,addOrNot: Boolean): Int = inputIntOne match{
+    case 0 if inputIntTwo!=0 => inputIntTwo
+    case _ => inputIntTwo match {
+        case 0 if inputIntOne!=0 => inputIntOne
+        case 0 if inputIntTwo==0 => 0
+        case _ =>  addOrNot match{
+            case addOrNot=> inputIntOne*inputIntTwo
+            case _ => inputIntOne+inputIntTwo
+          }
+      }
+
   }
-  println(integerAdditionPatMatch(2,3,false))
+  println(integerAdditionPatMatch(2,3,true))
 }
