45dc15b3a9590f71de65f4c5b11432ff8238d05c
JayKL <Jaykonrad-lee@hotmail.com>
2019-09-25 09:14:07 +0100

    integer Addition Pattern Match with multi

--- src/main/scala/day1/PatMatch.scala
+++ src/main/scala/day1/PatMatch.scala
@@ -1,8 +1,9 @@
 package day1
 
 object PatMatch extends App{
-  def integerAdditionPatMatch(inputIntOne: Int, inputIntTwo: Int): Int = inputIntOne match{
+  def integerAdditionPatMatch(inputIntOne: Int, inputIntTwo: Int,addOrNot: Boolean): Int = addOrNot match{
+    case addOrNot=> inputIntOne*inputIntTwo
     case _ => inputIntOne+inputIntTwo
   }
-  println(integerAdditionPatMatch(2,3))
+  println(integerAdditionPatMatch(2,3,false))
 }
