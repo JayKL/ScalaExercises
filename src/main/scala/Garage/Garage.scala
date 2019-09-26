716078059d268e560d06de1857b9c436fbb69018
JayKL <Jaykonrad-lee@hotmail.com>
2019-09-26 13:25:38 +0100

    beggining of stage 2 Garage

--- src/main/scala/Garage/Garage.scala
+++ src/main/scala/Garage/Garage.scala
@@ -1,8 +1,39 @@
 package Garage
 
 class Garage {
-  var listOfPeople: List[Person]
-  var listOfVehicles: List[Vehicle]
+  var listOfPeople: List[Person] = List()
+  var listOfVehicles: List[Vehicle] =  List()
 
+  def addVehicle(): Unit ={
+
+  }
+
+  def removeVehicle(): Unit ={
+
+  }
+
+  def registerEmployee: Unit ={
+
+  }
+
+  def fixVehicle: Unit ={
+
+  }
+
+  def calculateBill: Unit ={
+
+  }
+
+  def getContentsOfGarage: Unit ={
+
+  }
+
+  def openGarage: Unit ={
+
+  }
+
+  def closeGarage: Unit ={
+
+  }
 
 }
