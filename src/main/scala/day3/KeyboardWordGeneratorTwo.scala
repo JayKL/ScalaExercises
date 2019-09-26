3df0d984fae9a566aeb7cb9900ae86936f3a9e48
JayKL <Jaykonrad-lee@hotmail.com>
2019-09-26 09:13:00 +0100

    updated some of the logic, it was backwards for the finding the largest word.
    Added the extra task of finding the words witch are made up of at least one character.

---
+++ src/main/scala/day3/KeyboardWordGeneratorTwo.scala
@@ -0,0 +1,73 @@
+package day3
+
+import scala.io.Source
+
+object KeyboardWordGeneratorTwo extends App {
+
+  //def checkIfCharIsInString(inputString: String, inputChar: Char): Boolean = { removed for efficiency
+   // if (inputString.count(char => char.toString.matches(inputChar.toString)) == 0) {
+   //   false
+   // } else {
+   //   true
+  //  }
+ // }
+
+
+ // def checkIfArrayOfCharIsInString(inputString: String, inputCharArray: Array[Char]): Boolean = { removed for efficiency
+ //   var doesItContainAnyChar = false
+//    if (inputCharArray.count(char => checkIfCharIsInString(inputString, char)) == 0) {
+//      false
+ //   } else {
+ //     true
+//    }
+//  }
+
+ def checkIfSetOfCharIsInStringMoreE(inputString: String, inputSetChars: Set[Char]): Boolean = {
+   if (inputString.toSet.subsetOf(inputSetChars)) {
+     true
+   } else {
+     false
+   }
+ }
+
+  def checkIfSetsAreEqual(inputString: String, inputSetChars: Set[Char]): Boolean = {
+    if (inputString.toSet.subsetOf(inputSetChars) & inputSetChars.subsetOf(inputString.toSet)) {
+      true
+    } else {
+      false
+    }
+  }
+
+
+  def findLargestWord(listOfPossibleWords: List[String], charsToBeTested: Set[Char]): String = {
+  //  val arrayOfDifference = (Set('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')).diff(charsToBeTested) artifact of improvement, removed for efficiency
+    listOfPossibleWords.filter(word => checkIfSetOfCharIsInStringMoreE(word, charsToBeTested)).maxBy(_.length) //this one had arrayOfDifference
+  }
+
+  def findLargestWordsToList(listOfPossibleWords: List[String], charsToBeTested: Set[Char]): List[String] = {
+    listOfPossibleWords.filter(word => checkIfSetsAreEqual(word, charsToBeTested))
+  }
+
+  def listLargestWordsMadeByKeyboards(arrayOfCharsToCheck: Array[String], listOfPossibleWords: List[String]): List[String] = {
+    arrayOfCharsToCheck.map(stringToConvertToChars => findLargestWord(listOfPossibleWords,stringToConvertToChars.toCharArray.toSet)).toList
+  }
+
+  def listWordsMadeUpOfAtLeastOneCharacter(arrayOfCharsToCheck: Array[String], listOfPossibleWords: List[String]): List[List[String]] = {
+    arrayOfCharsToCheck.map(stringToConvertToChars => findLargestWordsToList(listOfPossibleWords,stringToConvertToChars.toCharArray.toSet)).toList
+  }
+
+  val listOfWordsToBeChecked: List[String] = Source.fromURL("http://norvig.com/ngrams/enable1.txt").mkString.split("\n").toList
+
+  println("Please Input Amount of Lines to read, then the chars that are available to the keyboard")
+  val arrayOfCharactersToCheck = Array.ofDim[String](scala.io.StdIn.readLine().toInt)
+  println("lines to read is " + arrayOfCharactersToCheck.size)
+  for (i <- 1 to arrayOfCharactersToCheck.size) {
+    println("please input characters to be checked")
+    arrayOfCharactersToCheck(i - 1) = scala.io.StdIn.readLine()
+  }
+  println("Finding largest words made by keyboards please wait")
+  listLargestWordsMadeByKeyboards(arrayOfCharactersToCheck, listOfWordsToBeChecked).foreach(word => println(word))
+  println("list of words made up at least one of each character")
+  listWordsMadeUpOfAtLeastOneCharacter(arrayOfCharactersToCheck,listOfWordsToBeChecked).foreach(word => println(word))
+
+}
