88692e671cb81d5a10d42c8902c4b7095d318f11
JayKL <Jaykonrad-lee@hotmail.com>
2019-09-30 13:32:17 +0100

    added multi test, cleaned code up

--- src/test/scala/Day5/CalculatorTest.scala
+++ src/test/scala/Day5/CalculatorTest.scala
@@ -4,21 +4,24 @@ import day5.Calculator
 import resources.ScalaExercisesTesting
 
 class CalculatorTest extends ScalaExercisesTesting {
+  var calcRefVar = new Calculator()
 
   "When creating Calculator reference variable" should "exist" in {
     assertCompiles("new Calculator()")
   }
 
   "When calling sum function" should "require two Integer inputs" in {
-    var calcRefVar = new Calculator()
     calcRefVar.sum(1,2)
   }
 
   "When calling sum function" should "return the summation of two Integer inputs" in {
-    var calcRefVar = new Calculator()
     assertResult(5){
       calcRefVar.sum(2,3)
     }
+  }
+
+  "When calling multiplication function" should "require two Integer Inputs" in {
+    calcRefVar.multi(2,3)
   }
 
 
