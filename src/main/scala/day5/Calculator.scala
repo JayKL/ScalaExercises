72879b9ffefe50c2f3043b65d93ddfa1afa2ee09
JayKL <Jaykonrad-lee@hotmail.com>
2019-09-30 16:39:32 +0100

    passed out of bounds multiplication tests

--- src/main/scala/day5/Calculator.scala
+++ src/main/scala/day5/Calculator.scala
@@ -4,15 +4,21 @@ class Calculator {
   def sum(inputIntOne: Int, inputIntTwo: Int): Any = {
     val inputIntOneLong = inputIntOne.toLong
     val inputIntTwoLong = inputIntTwo.toLong
-    inputIntOneLong + inputIntTwoLong match{
-      case resultMaxValueChecker: Long if resultMaxValueChecker>Integer.MAX_VALUE => "result out of bounds"
-      case resultMinValueChecker: Long if (-resultMinValueChecker)>Integer.MAX_VALUE => "result out of bounds"
+    inputIntOneLong + inputIntTwoLong match {
+      case resultMaxValueChecker: Long if resultMaxValueChecker > Integer.MAX_VALUE => "result out of bounds"
+      case resultMinValueChecker: Long if (-resultMinValueChecker) > Integer.MAX_VALUE => "result out of bounds"
       case _ => inputIntOne + inputIntTwo
     }
   }
 
-  def multi(inputIntOne: Int, inputIntTwo: Int): Int = {
-    inputIntOne * inputIntTwo
+  def multi(inputIntOne: Int, inputIntTwo: Int): Any = {
+    val inputIntOneLong = inputIntOne.toLong
+    val inputIntTwoLong = inputIntTwo.toLong
+    inputIntOneLong * inputIntTwoLong match {
+      case resultMaxValueChecker: Long if resultMaxValueChecker > Integer.MAX_VALUE => "result out of bounds"
+      case resultMinValueChecker: Long if (-resultMinValueChecker) > Integer.MAX_VALUE => "result out of bounds"
+      case _ => inputIntOne * inputIntTwo
+    }
   }
 
   def minus(inputIntOne: Int, inputIntTwo: Int): Int = {
