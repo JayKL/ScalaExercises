38f10d3a10690e76902789865717d274fb6c377b
JayKL <Jaykonrad-lee@hotmail.com>
2019-10-01 11:20:58 +0100

    passed test for entering a limit

--- src/main/scala/day6/PrimeNumbers.scala
+++ src/main/scala/day6/PrimeNumbers.scala
@@ -3,4 +3,7 @@ package day6
 class PrimeNumbers {
   val wheelHitPositions=List(1, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 49, 53, 59)
 
+  def findPrimeNumbersUpTo(limit: Int): Unit ={
+
+  }
 }
