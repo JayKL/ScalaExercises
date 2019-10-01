15e5fc0269792f6e20baa2b1d0f433f24b3899ce
JayKL <Jaykonrad-lee@hotmail.com>
2019-10-01 10:21:33 +0100

    Calculator clean up

--- src/main/scala/day5/Calculator.scala
+++ src/main/scala/day5/Calculator.scala
@@ -1,33 +1,45 @@
 package day5
 
 class Calculator {
+
+  val outOfBounds = "result out of bounds"
+
+  def upperBound(numberToBeChecked: Long) = {
+    if (numberToBeChecked > Integer.MAX_VALUE) true else false
+  }
+
+  def lowerBound(numberToBeChecked: Long): Boolean={
+    if (numberToBeChecked>Integer.MAX_VALUE) true else false
+  }
+
   def sum(inputIntOne: Int, inputIntTwo: Int): Any = {
     inputIntOne.toLong + inputIntTwo.toLong match {
-      case resultMaxValueChecker: Long if resultMaxValueChecker > Integer.MAX_VALUE => "result out of bounds"
-      case resultMinValueChecker: Long if (-resultMinValueChecker) > Integer.MAX_VALUE => "result out of bounds"
+      case resultMaxValueChecker: Long if upperBound(resultMaxValueChecker) => outOfBounds
+      case resultMinValueChecker: Long if lowerBound(resultMinValueChecker)  => outOfBounds
       case _ => inputIntOne + inputIntTwo
     }
   }
 
   def multi(inputIntOne: Int, inputIntTwo: Int): Any = {
     inputIntOne.toLong * inputIntTwo.toLong match {
-      case resultMaxValueChecker: Long if resultMaxValueChecker > Integer.MAX_VALUE => "result out of bounds"
-      case resultMinValueChecker: Long if (-resultMinValueChecker) > Integer.MAX_VALUE => "result out of bounds"
+      case resultMaxValueChecker: Long if upperBound(resultMaxValueChecker) => outOfBounds
+      case resultMinValueChecker: Long if lowerBound(resultMinValueChecker) => outOfBounds
       case _ => inputIntOne * inputIntTwo
     }
   }
 
   def minus(inputIntOne: Int, inputIntTwo: Int): Any = {
     inputIntOne.toLong - inputIntTwo.toLong match {
-      case resultMaxValueChecker: Long if resultMaxValueChecker > Integer.MAX_VALUE => "result out of bounds"
-      case resultMinValueChecker: Long if (-resultMinValueChecker) > Integer.MAX_VALUE => "result out of bounds"
+      case resultMaxValueChecker: Long if upperBound(resultMaxValueChecker)=> outOfBounds
+      case resultMinValueChecker: Long if lowerBound(resultMinValueChecker)=> outOfBounds
       case _ => inputIntOne - inputIntTwo
-    }  }
+    }
+  }
 
   def divide(numerator: Int, denominator: Int): Any = {
-    denominator match  {
+    denominator match {
       case 0 => "cannot divide by zero"
-      case _ =>  numerator / denominator
+      case _ => numerator / denominator
     }
   }
 }
