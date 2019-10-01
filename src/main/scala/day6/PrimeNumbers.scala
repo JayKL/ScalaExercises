b54d27ca68a973886c8e8598f699547a4f2d60b1
JayKL <Jaykonrad-lee@hotmail.com>
2019-10-01 15:27:26 +0100

    created new tests, updated old tests

--- src/main/scala/day6/PrimeNumbers.scala
+++ src/main/scala/day6/PrimeNumbers.scala
@@ -1,15 +1,16 @@
 package day6
 
 class PrimeNumbers {
-  val wheelHitPositions=List(1, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 49, 53, 59)
+  val wheelHitPositions=Array(1, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 49, 53, 59)
 
-  def findPrimeNumbersUpTo(startSieve:Int,limitSieve: Int): Unit ={
+  def findPrimeNumbersUpTo(startSieve:Long,limitSieve: Long): Unit ={
     createSieveList(startSieve,limitSieve)
   }
 
-  def createSieveList(startSieve:Int,limitSieve:Int): List[Long]={
-   List.range(startSieve,limitSieve)
+  def createSieveList(startSieve:Long,limitSieve:Long): Array[Long]={
+   Array.range(startSieve,limitSieve)
   }
+
 
 
 }
